faef115ada48a40e0307bea758dad62f2e4b0e0b
WraitheW <48034189+WraitheW@users.noreply.github.com>
2020-09-17 15:08:21 -0700

    Script Folder

---
+++ Scripts/Invoker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Invoker
+{
+    private Command comm;
+
+    public void SetCommand(Command command)
+    {
+        comm = command;
+    }
+
+    public void ExeCommand()
+    {
+        CommandLog.commands.Enqueue(comm);
+        comm.Exe();
+    }
+}
+
---
+++ Scripts/gameManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class gameManager : MonoBehaviour {
+
+    bool gameHasEnded = false;
+
+    public GameObject completeLevelUI;
+
+    public float restartDelay = 1.5f;
+
+    public void completeLevel()
+    {
+        completeLevelUI.SetActive(true);
+    }
+
+    public void endGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("Game Over");
+            Invoke("Restart", restartDelay);
+        }
+        
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
